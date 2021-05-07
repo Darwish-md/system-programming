4fc2d1a3e5025c9c8427857ea97f040ae1bae67e
Mahmoud Darwish <72353586+Darwish-md@users.noreply.github.com>
2021-05-06 17:28:57 -0700

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,5 +1,6 @@
 /*
-Write a C program, which continuously prints out integer numbers in increasing order started by 1. The program must terminate after 1 second. To implement the program use a timer and signaling.
+Write a C program, which continuously prints out integer numbers in increasing order started by 1. 
+The program must terminate after 1 second. To implement the program use a timer and signaling.
 */
 #include<stdio.h>
 #include <stdlib.h> 
@@ -25,7 +26,8 @@ int main(int argc, char* argv[]){
 
 //-------------------------------------
 /*
-There is a file containing integer numbers (each integer is followed by a newline character). The number of integers is unknown. The file is called “nums.txt”. Write a C program to calculate the sum of the integers and write it to the end of the file.
+There is a file containing integer numbers (each integer is followed by a newline character). 
+The number of integers is unknown. The file is called “nums.txt”. Write a C program to calculate the sum of the integers and write it to the end of the file.
 */
 #include <stdio.h>
 
@@ -54,7 +56,9 @@ fclose(f);
 
 //-------------------------------------
 /*
-Write a C program, in which you define a function without any parameter. The function randomly returns an English letter character. The probability of the lowercase letters must be the double than the probability of uppercase letters. (The character should not be the same for each run.) Call the function in the main program unit and print the returned value. (Hint: ‘a’ is 97 and ‘A’ is 65.)
+Write a C program, in which you define a function without any parameter. 
+The function randomly returns an English letter character. The probability of the lowercase letters must be the double than the probability of uppercase letters.
+(The character should not be the same for each run.) Call the function in the main program unit and print the returned value. (Hint: ‘a’ is 97 and ‘A’ is 65.)
 */
 #include<stdio.h>
 #include<stdlib.h>
@@ -86,7 +90,10 @@ int main(int argc, char argv[]){
 
 //-------------------------------------
 /*
-Write a C program, in which you define a procedure having 4 parameters. The first two parameters are integers, the first one plays the role of the dividend (numerator) and the second one is the divisor (dominator) of a division. The procedure must determine both the integer part of the quotient and reminder and they must be provided to the caller via parameter passing by pseudo-address using the third and fourth parameters as output parameters. Call the procedure using the 60 and 8 as input parameters and in the main program unit print out the results (7 and 4).
+Write a C program, in which you define a procedure having 4 parameters. The first two parameters are integers, the first one plays the role of the dividend (numerator)
+and the second one is the divisor (dominator) of a division. The procedure must determine both the integer part of the quotient and reminder and they must be provided to
+the caller via parameter passing by pseudo-address using the third and fourth parameters as output parameters. Call the procedure using the 60 and 8 as input parameters 
+and in the main program unit print out the results (7 and 4).
 */
 #include<stdio.h>
 #include <stdlib.h> 
